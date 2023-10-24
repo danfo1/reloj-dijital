093fa98bee2b493bfa82f29951e4fbaa90d6be6a
danfo1 <139786109+danfo1@users.noreply.github.com>
2023-10-24 13:53:47 -0500

    Add files via upload

---
+++ reloj/script.js
@@ -0,0 +1,16 @@
+function calcular(){
+
+    let tiempo = new Date();
+
+    let hora = tiempo.getHours();
+    let minutos = tiempo.getMinutes();
+    let segundos = tiempo.getSeconds();
+    hora = hora < 10 ? "0"+ hora:hora;
+    minutos = minutos < 10 ? "0"+ minutos:minutos;
+    segundos = segundos < 10 ? "0"+ segundos:segundos;
+
+    let reloj= hora +":"+minutos+":"+segundos;
+    let relojdigital =document.querySelector(".reloj");
+    relojdigital.innerHTML=reloj;
+}
+setInterval(calcular,1000);
